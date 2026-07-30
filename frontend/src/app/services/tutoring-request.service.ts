025a2c8b394cc335d8ca44abdd79db1fa7926fc0
Mahmoud-Eldiarby <mhmwdaldyarby44@gmail.com>
2026-07-30 07:38:06 +0200

    Refactor TutoringRequestService for API changes
    Updated API URL and added authorization headers for requests.

--- frontend/src/app/services/tutoring-request.service.ts
+++ frontend/src/app/services/tutoring-request.service.ts
@@ -1,21 +1,42 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TutoringRequestService {
 
-  private apiUrl = 'http://localhost:3000/tutoring-requests';
+  private apiUrl = 'http://localhost:5000/tutoring-requests';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) {}
+
+  private getHeaders() {
+    const token = this.authService.getToken();
+
+    return {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${token}`
+      })
+    };
+  }
 
   createRequest(topic: string): Observable<any> {
-    return this.http.post(this.apiUrl, { topic });
+    return this.http.post(
+      this.apiUrl,
+      { topic },
+      this.getHeaders()
+    );
   }
 
   getRequests(): Observable<any> {
-    return this.http.get(this.apiUrl);
+    return this.http.get(
+      this.apiUrl,
+      this.getHeaders()
+    );
   }
 }
